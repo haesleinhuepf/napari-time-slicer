fae7d13db5d94715dd40d48a7f6257406e9017d0
Robert Haase <haesleinhuepf@users.noreply.github.com>
2021-11-27 13:30:58 +0100

    layout

--- src/napari_time_slicer/_dock_widget.py
+++ src/napari_time_slicer/_dock_widget.py
@@ -35,24 +35,30 @@ class WorkflowWidget(QWidget):
         def update_layer(*_):
             from ._workflow import WorkflowManager, _layer_invalid, _viewer_has_layer
             workflow = WorkflowManager.install(napari_viewer).workflow
+            roots = workflow.roots()
 
             def build_output(list_of_items, func_to_follow, level=0):
                 output = ""
                 for i in list_of_items:
                     if _viewer_has_layer(self.viewer, i):
                         layer = self.viewer.layers[i]
-                        if _layer_invalid(layer):
-                            output = output + '<font color="#ff0000">'
+                        if layer.name in roots:
+                            output = output + '<font color="#dddddd">'
+                        elif _layer_invalid(layer):
+                            output = output + '<font color="#dd0000">'
                         else:
-                            output = output + '<font color="#00ff00">'
-                        output = output + ("   " * level) + "-> " + i + ""
-                        output = output + '</font>\n'
+                            output = output + '<font color="#00dd00">'
+                        output = output + ("   " * level) + "-> " + i + "\n"
+                        output = output + '</font>'
 
                         output = output + build_output(func_to_follow(i), func_to_follow, level + 1)
-                return "<html><pre>" + output + "</pre></html>"
+                return output
 
-            lbl_from_roots.setText(build_output(workflow.roots(), workflow.followers_of))
-            lbl_from_leafs.setText(build_output(workflow.leafs(), workflow.sources_of))
+            def html(text):
+                return "<html><pre>" + text + "</pre></html>"
+
+            lbl_from_roots.setText(html(build_output(workflow.roots(), workflow.followers_of)))
+            lbl_from_leafs.setText(html(build_output(workflow.leafs(), workflow.sources_of)))
 
         self.timer.start()
 
