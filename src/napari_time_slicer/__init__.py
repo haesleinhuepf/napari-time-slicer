447e6a3b7d82f0e79db490d8f47e1446b767dc10
Cryaaa <ryan.savill@web.de>
2022-05-06 14:20:41 +0200

    implementing a dask version of the time slicer

--- src/napari_time_slicer/__init__.py
+++ src/napari_time_slicer/__init__.py
@@ -10,6 +10,11 @@ from functools import wraps
 import time
 import inspect
 import numpy as np
+import dask.array as da
+from dask import delayed
+
+    
+    
 # most imports here are just for backwards compatbility
 from ._workflow import WorkflowManager, CURRENT_TIME_FRAME_DATA, _get_layer_from_data, _break_down_4d_to_2d_kwargs, _viewer_has_layer
 
@@ -51,6 +56,53 @@ def time_slicer(function: Callable) -> Callable:
 
     return worker_function
 
+@curry
+def time_slicer_dask(function: Callable) -> Callable:
+    @wraps(function)
+    def worker_function(*args, **kwargs):
+        args = list(args)
+        sig = inspect.signature(function)
+        # create mapping from position and keyword arguments to parameters
+        # will raise a TypeError if the provided arguments do not match the signature
+        # https://docs.python.org/3/library/inspect.html#inspect.Signature.bind
+        bound = sig.bind(*args, **kwargs)
+        # set default values for missing arguments
+        # https://docs.python.org/3/library/inspect.html#inspect.BoundArguments.apply_defaults
+        bound.apply_defaults()
+
+        # Retrieve the viewer parameter so that we can know which current timepoint is selected
+        viewer = None
+        for key, value in bound.arguments.items():
+            if isinstance(value, napari.Viewer):
+                viewer = value
+
+        if viewer is None:
+            pass
+        else:
+            # in case of 4D-data (timelapse) crop out the current 3D timepoint
+            if len(viewer.dims.current_step) == 4:
+                shape = viewer.dims.nsteps
+                lazy_processed_image = delayed(function)
+                lazy_arrays = []
+                for i in range(viewer.dims.nsteps[0]):
+                    _break_down_4d_to_2d_kwargs(bound.arguments, i, viewer)
+                    lazy_arrays.append(
+                        lazy_processed_image(*bound.args, **bound.kwargs)
+                    )
+                # Stack into one large dask.array
+                stack = da.stack(
+                    [
+                        da.from_delayed(delayed_reader, shape=shape, dtype=sample.dtype)
+                        for delayed_reader in lazy_arrays
+                    ], 
+                    axis=0)
+                return result
+
+        # call the decorated function
+        result = function(*bound.args, **bound.kwargs)
+        return result
+
+    return worker_function
 
 @curry
 def slice_by_slice(function: Callable) -> Callable:
