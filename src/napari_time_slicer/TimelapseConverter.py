5eb457bd675ea436b39168322ab558341511fab9
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2023-09-27 14:38:05 +0200

    codestyle

--- src/napari_time_slicer/TimelapseConverter.py
+++ src/napari_time_slicer/TimelapseConverter.py
@@ -1,9 +1,10 @@
 import numpy as np
-from napari.layers import Points, Surface, Image, Labels, Vectors
+from napari.layers import Points, Surface, Image, Labels, Vectors, Layer
+
 
 class TimelapseConverter:
     """
-    This class allows converting napari 4D layer data between different formats.
+    Allows converting napari 4D layer data between different formats.
     """
     def __init__(self):
 
@@ -21,9 +22,10 @@ class TimelapseConverter:
             'napari.types.SurfaceData': self._list_of_surfaces_to_surface,
             'napari.types.ImageData': self._list_of_images_to_image,
             'napari.types.LabelsData': self._list_of_images_to_image,
-            'napari.types.VectorsData': self._list_of_vectors_to_vectors}
+            'napari.types.VectorsData': self._list_of_vectors_to_vectors,
+            Layer: self._list_of_layers_to_layer}
 
-        # This list of aliases allows to map LayerDataTuples to the correct napari.types
+        # Map LayerDataTuples to the correct napari.types
         self.tuple_aliases = {
             'points': 'napari.types.PointsData',
             'surface': 'napari.types.SurfaceData',
@@ -34,10 +36,13 @@ class TimelapseConverter:
             Surface: 'napari.types.SurfaceData',
             Image: 'napari.types.ImageData',
             Labels: 'napari.types.LabelsData',
-            Vectors: 'napari.types.VectorsData'}
+            Vectors: 'napari.types.VectorsData',
+            Layer: 'napari.layers.Layer'}
 
-        self.supported_4d_data = list(self.list_to_data_conversion_functions.keys())
-        self.supported_list_data = list(self.data_to_list_conversion_functions.keys())
+        self.supported_list_data = list(
+            self.list_to_data_conversion_functions.keys())
+        self.supported_4d_data = list(
+            self.data_to_list_conversion_functions.keys())
 
     def convert_4d_data_to_list(self, data, layertype: type) -> list:
         """
@@ -61,7 +66,8 @@ class TimelapseConverter:
 
         """
         if layertype not in self.supported_4d_data:
-            raise TypeError(f'{layertype} data to list conversion currently not supported.')
+            raise TypeError(f'{layertype} data to list conversion ' +
+                            'currently not supported.')
 
         conversion_function = self.data_to_list_conversion_functions[layertype]
         return conversion_function(data)
@@ -88,7 +94,8 @@ class TimelapseConverter:
 
         """
         if layertype not in self.supported_list_data:
-            raise TypeError(f'{layertype} list to data conversion currently not supported.')
+            raise TypeError(f'{layertype} list to data conversion ' +
+                            'currently not supported.')
         conversion_function = self.list_to_data_conversion_functions[layertype]
         return conversion_function(data)
 
@@ -96,13 +103,15 @@ class TimelapseConverter:
     # Images
     # =============================================================================
 
-    def _image_to_list_of_images(self, image: 'napari.types.ImageData') -> list:
+    def _image_to_list_of_images(
+            self, image: 'napari.types.ImageData') -> list:
         """Convert 4D image to list of images"""
         while len(image.shape) < 4:
             image = image[np.newaxis, :]
         return list(image)
 
-    def _list_of_images_to_image(self, images: list) -> 'napari.types.ImageData':
+    def _list_of_images_to_image(
+            self, images: list) -> 'napari.types.ImageData':
         """Convert a list of 3D image data to single 4D image data."""
         while len(images[0].shape) < 3:
             images = [image[np.newaxis, :] for image in images]
@@ -117,7 +126,9 @@ class TimelapseConverter:
         import pandas as pd
 
         # create dataframe for each set of points
-        dataframes = [pd.DataFrame(frame, columns=['z', 'y', 'x']) for frame in points]
+        dataframes = [
+            pd.DataFrame(frame, columns=['z', 'y', 'x']) for frame in points
+            ]
 
         # add frame column as first column to each dataframe
         for idx, df in enumerate(dataframes):
@@ -128,7 +139,8 @@ class TimelapseConverter:
 
         return points_out
 
-    def _points_to_list_of_points(self, points: 'napari.types.PointsData') -> list:
+    def _points_to_list_of_points(
+            self, points: 'napari.types.PointsData') -> list:
         """Convert a 4D point array to list of 3D points"""
         import pandas as pd
 
@@ -140,7 +152,9 @@ class TimelapseConverter:
         df = pd.DataFrame(points, columns=['t', 'z', 'y', 'x'])
 
         # group by frame index and return list of points using groupby
-        points_out = [group[['z', 'y', 'x']].values for _, group in df.groupby('t')]
+        points_out = [
+            group[['z', 'y', 'x']].values for _, group in df.groupby('t')
+            ]
 
         return points_out
 
@@ -148,7 +162,8 @@ class TimelapseConverter:
     # Vectors
     # =============================================================================
 
-    def _vectors_to_list_of_vectors(self, vectors: 'napari.types.VectorsData') -> list:
+    def _vectors_to_list_of_vectors(
+            self, vectors: 'napari.types.VectorsData') -> list:
         base_points = vectors[:, 0]
         vectors = vectors[:, 1]
 
@@ -157,10 +172,13 @@ class TimelapseConverter:
         vector_list = self._points_to_list_of_points(vectors)
 
         output_vectors = [
-            np.stack([pt, vec]).transpose((1, 0, 2)) for pt, vec in zip(point_list, vector_list)]
+            np.stack([pt, vec]).transpose((1, 0, 2)) for pt, vec in zip(
+                point_list, vector_list)
+                ]
         return output_vectors
 
-    def _list_of_vectors_to_vectors(self, vectors: list) -> 'napari.types.VectorsData':
+    def _list_of_vectors_to_vectors(
+            self, vectors: list) -> 'napari.types.VectorsData':
         base_points = [v[:, 0] for v in vectors]
         directions = [v[:, 1] for v in vectors]
 
@@ -174,7 +192,8 @@ class TimelapseConverter:
     # Surfaces
     # =============================================================================
 
-    def _surface_to_list_of_surfaces(self, surface: 'napari.types.SurfaceData') -> list:
+    def _surface_to_list_of_surfaces(
+            self, surface: 'napari.types.SurfaceData') -> list:
         """Convert a 4D surface to list of 3D surfaces"""
         import pandas as pd
 
@@ -194,7 +213,9 @@ class TimelapseConverter:
         # put points in dataframe and group by frame index
         # return list of points using groupby
         df_points = pd.DataFrame(points, columns=['t', 'z', 'y', 'x'])
-        points_out = [group[['z', 'y', 'x']].values for _, group in df_points.groupby('t')]
+        points_out = [
+            group[['z', 'y', 'x']].values for _, group in df_points.groupby('t')
+            ]
 
         # put faces in dataframe and determine frame for each face
         df_faces = pd.DataFrame(faces, columns=['p1', 'p2', 'p3'])
@@ -202,7 +223,9 @@ class TimelapseConverter:
 
         # group by frame index and return list of faces using groupby
         # subtract minimum point index from faces to make sure they start at 0
-        faces_out = [group[['p1', 'p2', 'p3']].values for _, group in df_faces.groupby('t')]
+        faces_out = [
+            group[['p1', 'p2', 'p3']].values for _, group in df_faces.groupby('t')
+            ]
         faces_out = [f - f.min() for f in faces_out]
 
         # if values were assigned to the surface, put them in dataframe and
@@ -210,12 +233,16 @@ class TimelapseConverter:
         if has_values:
             df_values = pd.DataFrame(values, columns=['values'])
             df_values['t'] = df_points['t'].iloc[df_values.index].values
-            values_out = [group['values'].values for _, group in df_values.groupby('t')]
+            values_out = [
+                group['values'].values for _, group in df_values.groupby('t')
+                ]
 
         # put together points, faces and values (if available) for each frame
         # and return list of surfaces
         if has_values:
-            surfaces_out = [(p, f, v) for p, f, v in zip(points_out, faces_out, values_out)]
+            surfaces_out = [
+                (p, f, v) for p, f, v in zip(points_out, faces_out, values_out)
+                ]
         else:
             surfaces_out = [(p, f) for p, f in zip(points_out, faces_out)]
 
@@ -232,7 +259,7 @@ class TimelapseConverter:
 
         # Surfaces do not necessarily have values - check if this is the case
         if len(surfaces[0]) == 3:
-            values = np.concatenate([surface[2] for surface in surfaces])  # retrieve values if existant
+            values = np.concatenate([surface[2] for surface in surfaces])
         else:
             values = None
 
